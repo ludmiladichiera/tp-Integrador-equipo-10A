c907d9be90144e70928e2afec643817ced769d50
ludmiladichiera <ludmiladichiera@gmail.com>
2025-05-31 14:39:08 -0300

    clase imagen

--- Dominio/Producto.cs
+++ Dominio/Producto.cs
@@ -10,11 +10,11 @@ namespace Dominio
     {
         public int Id { get; set; }
         public string Codigo { get; set; }
+        public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public decimal Precio { get; set; }
         public int Stock { get; set; }
-        public string UnidadVenta { get; set; } // unidad, docena
-        public string ImagenUrl { get; set; }
+        public string UnidadVenta { get; set; } // unidad, docena  
 
         public Categoria Categoria { get; set; }
     }
